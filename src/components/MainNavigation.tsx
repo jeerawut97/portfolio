b450a39b401008c10af94c16a8b91803b0bdd338
JEE <jeerawut.jd40@gmail.com>
2024-02-08 18:44:35 +0700

    Update UI

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,19 +1,23 @@
 import { NavLink } from "react-router-dom";
 
 import classes from './MainNavigation.module.css'
+import img from '../assets/vite.svg'
 
 function MainNavigation() {
     return (
         <header className={classes.header}>
+            <div>
+                <img src={img} alt="icon-header" height="75px" width="100px"/>
+            </div>
             <nav>
                 <ul className={classes.list}>
                     <li>
-                        <NavLink to="/portfolio" className={({isActive}) => isActive? classes.active: undefined}>Portfolio</NavLink>
+                        <NavLink to="/" className={({isActive}) => isActive? classes.active: undefined} end>
+                            Home
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/" className={({isActive}) => isActive? classes.active: undefined} end>
-                            My Home
-                        </NavLink>
+                        <NavLink to="/portfolio" className={({isActive}) => isActive? classes.active: undefined}>Portfolio</NavLink>
                     </li>
                     <li>
                         <NavLink to="/about-us" className={({isActive}) => isActive? classes.active: undefined}>About Us</NavLink>
