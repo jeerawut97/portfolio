8b7482eb8ded4e79be8f8ca6bf5b1feebd3d04c8
JRW <jeerawut.jd40@gmail.com>
2024-02-24 12:56:38 +0700

    Update - Skelton UI - Structure Project

--- frontend/src/components/MainNavigation.tsx
+++ frontend/src/components/MainNavigation.tsx
@@ -6,14 +6,11 @@ import img from '../assets/vite.svg'
 function MainNavigation() {
     return (
         <header className={classes.header}>
-            <NavLink to="/" end><img src={img} alt="icon-header" height="75px" width="100px"/></NavLink>
+            <NavLink to="/" end><img src={img} alt="icon-header" height="120px" width="120px"/></NavLink>
             <nav>
                 <ul className={classes.list}>
                     <li>
                         <NavLink to="/portfolio" className={({isActive}) => isActive? classes.active: undefined}>Portfolio</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about-us" className={({isActive}) => isActive? classes.active: undefined}>About Us</NavLink>
                     </li>
                 </ul>
             </nav>
