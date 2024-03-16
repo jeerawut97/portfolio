4f2061577fdcd82f489fbdd2458103549cc6ce3d
JEE <jeerawut.jd40@gmail.com>
2024-03-16 12:53:40 +0700

    fix bug carousel don't slide

--- app/components/carousel/Carousel.tsx
+++ app/components/carousel/Carousel.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 
-const Carousel = async ({ images }: any) => {
+const Carousel = ({ images }: any) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState("");
 
