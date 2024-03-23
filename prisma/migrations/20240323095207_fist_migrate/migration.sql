8f7b6acfbb24b0f23b7a3c071484bd3841fd6fea
JEE <jeerawut.jd40@gmail.com>
2024-03-23 20:28:23 +0700

    change generated

--- prisma/migrations/20240323095207_fist_migrate/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "portfolio" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL
-);
