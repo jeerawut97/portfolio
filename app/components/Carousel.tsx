2cb4744c94c5b5739c77e40757808f0f13c998b7
JEE <jeerawut.jd40@gmail.com>
2024-04-20 23:47:11 +0700

    Redesign to lib 3rd party ant

---
+++ app/components/Carousel.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import React from 'react';
+import { Carousel } from 'antd';
+
+const contentStyle: React.CSSProperties = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+};
+
+const UICarousel: React.FC = () => (
+  <Carousel autoplay>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>
+);
+
+export default UICarousel;
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,56 @@
 'use client'
-import Image from 'next/image'
 
-const img = <Image
-                src="/eiei.png"
-                alt='me'
-                width={0}
-                height={0}
-                sizes="200vw"
-                style={{ width: '100%' }}
-                priority
-            />
+import React from 'react';
+import { Breadcrumb, Layout, theme } from 'antd';
+import UICarousel from './components/Carousel';
 
-async function HomePage() {
-    return (
-        <>
-            <div>
-                <div>{img}</div>
-                <div><p>As a developer, I'm Jeerawut Jaidee embarked on a coding journey fueled by an unwavering passion for software development.</p></div>
+const { Content, Footer } = Layout;
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <>
+        <Layout style={{backgroundColor: '#242424'}}>
+        <Content style={{ padding: '0 48px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }} />
+            <div
+            style={{
+                padding: 24,
+                minHeight: 380,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+            }}
+            >
+            Content
             </div>
-        </>
-    )
-}
+        </Content>
+        <Content style={{ padding: '0 48px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }} />
+            <UICarousel /> 
+        </Content>
+        <Content style={{ padding: '0 48px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }} />
+            <div
+            style={{
+                padding: 24,
+                minHeight: 380,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+            }}
+            >
+            Content
+            </div>
+        </Content>
+        <Footer style={{ textAlign: 'center', backgroundColor: '#242424', color: 'white'}}>
+            Ant Design ©{new Date().getFullYear()}
+        </Footer>
+        </Layout>
+    </>
+  );
+};
 
-export default HomePage;
+export default App;
+
