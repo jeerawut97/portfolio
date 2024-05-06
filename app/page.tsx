991a854e10de372c5917dfe082c9d5e9bbd7e26b
JEE <jeerawut.jd40@gmail.com>
2024-05-07 03:38:56 +0700

    auto min height

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ const App: React.FC = () => {
               <div
               style={{
                   padding: 24,
-                  minHeight: 200,
+                  minHeight: 'auto',
                   background: colorBgContainer,
                   borderRadius: borderRadiusLG,
               }}
